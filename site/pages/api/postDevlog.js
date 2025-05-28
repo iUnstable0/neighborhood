5a59699e3ad3597c4ce7a8a0401e87a05b7ed2bd
SerenityUX <thomas@serenidad.app>
2025-05-28 14:44:35 -0700

    devlog change

--- site/pages/api/postDevlog.js
+++ site/pages/api/postDevlog.js
@@ -18,9 +18,9 @@ async function createMoleCheck(appLink, githubUrl) {
       })
     });
     
-    // if (!response.ok) {
-    //   throw new Error(`HTTP error! status: ${response.status}`);
-    // }
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
     
     return await response.json();
   } catch (error) {
