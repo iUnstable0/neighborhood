5e759a5aa8e0ba9fd1ef229d9c577b4ec576e270
SerenityUX <thomas@serenidad.app>
2025-06-11 15:50:18 -0700

    ok cool

--- site/pages/HacktendoGrid.js
+++ site/pages/HacktendoGrid.js
@@ -74,7 +74,7 @@ export default function HacktendoGrid({
                   padding: 0,
                   flexDirection: imageUrl ? 'unset' : 'column',
                 }}
-                onClick={() => openAppLink(game)}
+                onClick={() => game.appLink && openAppLink(game)}
                 >
                   {imageUrl ? (
                     <img
